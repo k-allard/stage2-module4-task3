d301cea7f89f8608a75e8ea2224202d5ad34fe04
Anastasiia Smirnova <smirn.anastasiia@gmail.com>
2023-05-23 19:02:17 +0400

    Fix typo 2 in task 2

--- task2-observer-pattern/src/main/java/com/mjc/stage2/impl/RectangleObserver.java
+++ task2-observer-pattern/src/main/java/com/mjc/stage2/impl/RectangleObserver.java
@@ -11,7 +11,7 @@ public class RectangleObserver implements Observer {
     public void handleEvent(RectangleEvent event) {
         Rectangle rectangle = event.getSource();
         double side = rectangle.getSideA();
-        double side2 = rectangle.getSideA();
+        double side2 = rectangle.getSideB();
         RectangleWarehouse.getInstance().put(rectangle.getId(),
                 new RectangleValues(side * side2, (side * 2) + (side2 * 2)));
     }
